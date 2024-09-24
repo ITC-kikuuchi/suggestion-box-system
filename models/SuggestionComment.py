169bb768cf5ca5930ed106d78df48c131fb5cc4d
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-24 13:20:51 +0900

    feat: モデルクラス SuggestionComment の実装

---
+++ models/SuggestionComment.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class SuggestionComment(Base):
+    __tablename__ = "t_suggestion_comment"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    suggestion_id = Column(
+        Integer, ForeignKey("t_suggestion.id"), nullable=False
+    )
+    comment = Column(String(255), nullable=False)
+    created_id = Column(Integer)
+    updated_id = Column(Integer)
+    created_at = Column(Timestamp)
+    updated_at = Column(Timestamp)
+
+    suggestion = relationship("suggestion", back_populates="suggestionComment")
