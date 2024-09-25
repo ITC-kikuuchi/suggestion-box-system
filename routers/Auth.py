64e0b1f7eec7fc7a3b3268e69b9c09c9bff09ad0
kikuuchi <kikuuchi@itcowork.co.jp>
2024-09-25 10:26:23 +0900

    feat: トークン生成処理の実装

--- routers/Auth.py
+++ routers/Auth.py
@@ -14,6 +14,27 @@ import cruds.User as UserCrud
 
 router = APIRouter()
 
+# OAuth2PasswordBearerを使用してトークン取得用エンドポイントを設定
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# トークン生成処理
+def createTokens(user):
+    # ペイロード生成
+    accessPayload = {
+        "token_type": "access_token",
+        "exp": datetime.utcnow() + timedelta(minutes=60),
+        "user_id": user.id,
+    }
+    # アクセストークンの生成
+    accessToken = jwt.encode(accessPayload, os.environ["SECRET_KEY"], algorithm="HS256")
+    # アクセストークンの返却
+    return {
+        "id": user.id,
+        "unknown": user.unknown,
+        "access_token": accessToken,
+    }
+
 
 # ログインAPI
 @router.post("/login")
