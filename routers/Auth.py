a7192a4e75d1272433b9c2e1e2b459712203ada9
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-01 11:19:55 +0900

    feat: トークンを検証してユーザを取得する処理の実装

--- routers/Auth.py
+++ routers/Auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
 from database import get_db
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 
 from Exceptions.UnauthorizedException import UnauthorizedException
 from utils.ExceptionHandler import exception_handler
@@ -40,6 +40,23 @@ def getBearerToken(authorization: str = Header(None)):
     if not authorization.startswith("Bearer "):
         raise UnauthorizedException
     return authorization.split(" ")[1]
+
+
+# トークンを検証してユーザーを取得
+def getCurrentUser(token: str = Depends(getBearerToken), db: Session = Depends(get_db)):
+    try:
+        # トークンをデコードしてペイロードを取得
+        payload = jwt.decode(token, os.environ["SECRET_KEY"], algorithms=["HS256"])
+        # ID に紐づくユーザ情報の取得
+        user = UserCrud.getUserById(db, payload["user_id"])
+        if not user:
+            # ID に紐づくユーザ情報が存在しない場合
+            raise UnauthorizedException
+        return user
+    except JWTError:
+        raise UnauthorizedException
+    except Exception as e:
+        return exception_handler(e)
 
 
 # ログインAPI
