85d6c3c60dab621e9658a70e961149b28b71c59a
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-02 14:15:09 +0900

    refactor: コード整形

--- cruds/Status.py
+++ cruds/Status.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session, joinedload
 
 from models.Status import Status
 
+
 # ステータス一覧取得
 def getStatuses(db: Session):
     return db.query(Status).options(joinedload(Status.suggestion)).all()
