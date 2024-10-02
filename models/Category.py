ed761bb9832d88bb3eaf03f5f223702302e74c45
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-02 11:03:55 +0900

    fix: リレーションの修正及び status_id カラムの追加

--- models/Category.py
+++ models/Category.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -13,3 +14,5 @@ class Category(Base):
     updated_id = Column(Integer)
     created_at = Column(Timestamp)
     updated_at = Column(Timestamp)
+
+    suggestionCategory = relationship("SuggestionCategory", back_populates="category")
