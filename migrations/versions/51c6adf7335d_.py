51fe02313054913724888deebcaa65de905fdcf0
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-02 11:04:38 +0900

    fix: マイグレーションファイルの修正

--- migrations/versions/51c6adf7335d_.py
+++ migrations/versions/51c6adf7335d_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8004c2001892
+Revision ID: 51c6adf7335d
 Revises: 
-Create Date: 2024-09-24 04:14:14.621942
+Create Date: 2024-10-02 02:00:28.994142
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision: str = '8004c2001892'
+revision: str = '51c6adf7335d'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -55,11 +55,12 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('title', sa.String(length=255), nullable=False),
     sa.Column('contents', sa.Text(), nullable=False),
-    sa.Column('is_resolved', sa.Boolean(), nullable=False),
+    sa.Column('status_id', sa.Integer(), nullable=False),
     sa.Column('created_id', sa.Integer(), nullable=True),
     sa.Column('updated_id', sa.Integer(), nullable=True),
     sa.Column('created_at', mysql.TIMESTAMP(), nullable=True),
     sa.Column('updated_at', mysql.TIMESTAMP(), nullable=True),
+    sa.ForeignKeyConstraint(['status_id'], ['m_status.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('t_suggestion_category',
