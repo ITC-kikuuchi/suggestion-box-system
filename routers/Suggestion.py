e7d75c9c834e2f6ce59653ebf1ddfde46c34c245
kikuuchi <kikuuchi@itcowork.co.jp>
2024-10-27 20:11:58 +0900

    feat: 意見詳細取得API の実装

--- routers/Suggestion.py
+++ routers/Suggestion.py
@@ -85,6 +85,41 @@ async def getSuggestionDetail(
     suggestion_id: int,
     loginUser: dict = Depends(getCurrentUser), db: Session = Depends(get_db)
 ):
+    try:
+        # 意見一覧取得
+        suggestion = SuggestionCrud.getSuggestionDetail(db, suggestion_id)
+        # カテゴリ一覧の作成
+        categoryList = []
+        for category in suggestion.suggestionCategory:
+            categoryList.append(
+                {
+                    "category_id": category.category.id,
+                    "category": category.category.category,
+                }
+            )
+        # コメント一覧の作成
+        commentList = []
+        for comment in suggestion.suggestionComment:
+            commentList.append(
+                {
+                    "comment_id": comment.id ,
+                    "comment": comment.comment,
+                    "created_id": comment.created_id,
+                }
+            )
+        # レスポンスの作成
+        response = SuggestionSchema.SuggestionDetail(
+            id=suggestion.id,
+            unknown=UNKNOWN,
+            title=suggestion.title,
+            created_at=suggestion.created_at.strftime(DATE_FORMAT_YMD),
+            status_id=suggestion.status_id,
+            category_list=categoryList,
+            comment_list=commentList,
+        )
+    except Exception as e:
+        return exception_handler(e)
+    return response
 
 
 # 意見削除API
