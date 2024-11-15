9c7a4a44cc401e9b4fd1fee708c0fbbf86e986b9
kikuuchi <kikuuchi@itcowork.co.jp>
2024-11-16 00:20:22 +0900

    feat: コメント登録API の実装

--- routers/SuggestionComment.py
+++ routers/SuggestionComment.py
@@ -15,8 +15,30 @@ import cruds.SuggestionComment as SuggestionCommentCrud
 
 # コメント登録API
 @router.post("/comments")
-async def createComment():
-    pass
+async def createComment(
+    item: SuggestionCommentSchema.CreateComment,
+    loginUser: dict = Depends(getCurrentUser),
+    db: Session = Depends(get_db),
+):
+    response = []
+    try:
+        # 意見詳細取得
+        suggestion = SuggestionCrud.getSuggestionDetail(db, item.suggestion_id)
+        # データ存在チェック
+        if not suggestion:
+            # id に紐づくデータが存在しなかった場合
+            raise NotFoundException
+
+        comment = {
+            "suggestion_id": item.suggestion_id,
+            "comment": item.comment,
+            "created_id": UNKNOWN_CREATED_ID,
+        }
+        # コメント登録
+        SuggestionCommentCrud.createComment(db, comment)
+    except Exception as e:
+        return exception_handler(e)
+    return response
 
 
 # コメント更新API
