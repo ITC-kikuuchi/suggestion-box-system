2e9b33f988d968bcf1b50943bded0e8492f06c14
kikuuchi <kikuuchi@itcowork.co.jp>
2024-11-16 00:20:03 +0900

    feat: import 処理の実装

--- routers/SuggestionComment.py
+++ routers/SuggestionComment.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from Exceptions.NotFoundException import NotFoundException
+from utils.ExceptionHandler import exception_handler
+from utils.CheckToken import getCurrentUser
+
+from constants import UNKNOWN_CREATED_ID
 
 router = APIRouter()
 
+import schemas.SuggestionComment as SuggestionCommentSchema
+import cruds.Suggestion as SuggestionCrud
+import cruds.SuggestionComment as SuggestionCommentCrud
 
 # コメント登録API
 @router.post("/comments")
