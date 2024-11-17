ac268d5eae92fcf045dfad9f11988688e2f1a64b
kikuuchi <kikuuchi@itcowork.co.jp>
2024-11-17 14:35:44 +0900

    feat: コメント詳細取得処理の実装

--- cruds/SuggestionComment.py
+++ cruds/SuggestionComment.py
@@ -8,3 +8,11 @@ def createComment(db: Session, comment):
     db.add(newSuggestionComment)
     db.commit()
     db.refresh(newSuggestionComment)
+
+# コメント詳細取得
+def getCommentDetail(db: Session, comment_id):
+    return (
+        db.query(SuggestionComment)
+        .filter(SuggestionComment.id == comment_id)
+        .first()
+    )
