db9fd18bec7a0c0eda078739ef9707f7a2252491
kikuuchi <kikuuchi@itcowork.co.jp>
2024-11-23 17:12:03 +0900

    refactor: コード整形

--- cruds/SuggestionComment.py
+++ cruds/SuggestionComment.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 
 from models.SuggestionComment import SuggestionComment
 
+
 # コメント登録
 def createComment(db: Session, comment):
     newSuggestionComment = SuggestionComment(**comment)
@@ -9,13 +10,13 @@ def createComment(db: Session, comment):
     db.commit()
     db.refresh(newSuggestionComment)
 
+
 # コメント詳細取得
 def getCommentDetail(db: Session, comment_id):
     return (
-        db.query(SuggestionComment)
-        .filter(SuggestionComment.id == comment_id)
-        .first()
+        db.query(SuggestionComment).filter(SuggestionComment.id == comment_id).first()
     )
+
 
 # コメント更新
 def updateComment(db: Session, comment, original: SuggestionComment):
@@ -23,6 +24,7 @@ def updateComment(db: Session, comment, original: SuggestionComment):
         setattr(original, field, value)
     db.commit()
     db.refresh(original)
+
 
 # コメント削除
 def deleteComment(db: Session, commentId):
